db266052d0d2cf2f9aca1ee44649dc100f806b40
Kishor Subedi <kishorsubedi@Kishors-MacBook-Pro.local>
2020-05-17 17:16:35 -0400

    username and password reached method

---
+++ build/precache-manifest.e678b2ffaf44556aae152fc0630dd166.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e84fd364aee76c6ba9e18a0b552185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1587f3fa533764f13fb2",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e8c84e73d9235a7a9c96",
+    "url": "/static/css/main.08a1e26e.chunk.css"
+  },
+  {
+    "revision": "1587f3fa533764f13fb2",
+    "url": "/static/js/2.68344a8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68344a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c84e73d9235a7a9c96",
+    "url": "/static/js/main.abc62344.chunk.js"
+  },
+  {
+    "revision": "f240a50342768bf56c2f",
+    "url": "/static/js/runtime-main.c41b6c6e.js"
+  },
+  {
+    "revision": "3266febc5add6ba2fd195db5f4760ae9",
+    "url": "/static/media/ppace.3266febc.png"
+  }
+]);
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(){
+        super();
+        this.state={username:'',password:''}
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name] : e.target.value });
+    }
+
     render() {
         return (
-            <form>
+            <React.Fragment>
 
                 <div className="form-group">
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="username" name="username" value={this.state.username} className="form-control" placeholder="Enter email" onChange={this.handleChange.bind(this)} />
                 </div>
 
                 <div className="form-group">
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" name="password" value={this.state.password} className="form-control" placeholder="Enter password"  onChange={this.handleChange.bind(this)}/>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Login</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick={(event) => this.handleClick(event)}>Login</button>
 
-            </form>
+            </React.Fragment>
         );
+    }
+
+    handleClick(event){
+        console.log(this.state.username);
+        console.log(this.state.password);
     }
 }
